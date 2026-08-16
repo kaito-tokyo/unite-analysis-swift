bfc363ccdb95fd65eb09d1ee03927d220b3773f5
Kaito Udagawa <umireon@kaito.tokyo>
2026-08-16 19:15:05 +0900

    Prepare v0.2.2 release
    Update the package, application bundle, command-line tools, installation documentation, and version test to v0.2.2.
    
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- Tests/LDTXRecordingSupportTests/ModelToolTests.swift
+++ Tests/LDTXRecordingSupportTests/ModelToolTests.swift
@@ -8,7 +8,7 @@ import Testing
 import UniteAnalysisModelCommands
 
 @Test func modelToolVersionIsHandledAsBuiltInOutput() {
-  #expect(modelBuiltInCLIOutput(arguments: ["--version"]) == "0.2.1")
+  #expect(modelBuiltInCLIOutput(arguments: ["--version"]) == "0.2.2")
 }
 
 @Test func modelToolHelpDescribesBuildCommand() {
